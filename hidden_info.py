7c837a4e42a93f758b7fb6f79a5d89906eaa66c9
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-03-18 20:45:02 +0200

    prepared weather forecast to send each morning. will be loaded on pythonanywhere

---
+++ hidden_info.py
@@ -0,0 +1,7 @@
+api_key = "9e370d219895a792e47c1a31aeec31cb"
+MY_LAT = 55.66
+MY_LNG = 21.18
+email = "test.sokolkin@gmail.com"
+password = "vntdxpgbnjsmwjoj"
+port = 587
+smtp_info = "smtp.gmail.com"
